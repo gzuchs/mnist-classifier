8ec93b76265e36aea8f1f5fc1895ecc380229a1f
Greg <gzucaro1@gmail.com>
2022-04-20 01:18:28 -0400

    Creating class that trains a supplied network to recognize 3s or 7s from the MNIST dataset. The fit method accepts a training DataLoader object, a validation DataLoader object, a model specification, an optimizer function declaration, a loss function, and an accuracy function. Also includes test script that creates model using sample optimizer, loss function, and accuracy function.

---
+++ mnist_learner.py
@@ -0,0 +1,101 @@
+import torch
+
+class Learner:
+    """Class to fit a two-layer neural network.
+    """
+    def __init__(self, training_dataset, validation_dataset, model, opt_func=None, loss_func=None, metrics=None):
+        """Creates object.
+        
+        dataloaders = DataLoader containing training set DataLoader [0] and validation set DataLoader [1]
+        model = PyTorch module that returns outcome values
+        
+        """
+        self.training_dataset = training_dataset
+        self.validation_dataset = validation_dataset
+        
+        self.model = model
+        
+        if opt_func is None:
+            opt_func = torch.optim.SGD
+            
+        self.optimizer_class = opt_func
+        
+        if loss_func is None:
+            loss_func = torch.nn.NLLLoss()
+            
+        self.loss_function = loss_func
+        
+        if metrics is None:
+            metrics = lambda xb, yb: ((xb.sigmoid() > 0.5) == yb).float().mean()
+            
+        self.metric_function = metrics
+        
+        self.epoch_training_loss = []
+        self.epoch_validation_loss = []
+        self.epoch_metrics = []
+
+
+    def _calculate_gradient(self, x_batch, y_batch):
+        predictions = self.model(x_batch)
+        loss = self.loss_function(predictions, y_batch)
+        loss.backward()
+        
+        return loss
+
+
+    def _calculate_validation_loss_metric(self, x_batch, y_batch):
+        predictions = self.model(x_batch)
+        loss = self.loss_function(predictions, y_batch)
+        
+        metric = self.metric_function(predictions, y_batch)
+        
+        return loss.item(), metric.item()
+
+
+    def _train_epoch(self):
+        # train_dl = torch.utils.data.DataLoader(self.training_dataset, batch_size=256)
+        
+        train_losses = 0
+        
+        for x_batch, y_batch in self.training_dataset:
+            loss = self._calculate_gradient(x_batch, y_batch)
+            train_losses += loss.item()
+            
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+        
+        epoch_train_loss = round(train_losses / len(self.training_dataset), 4)
+        self.epoch_training_loss.append(epoch_train_loss)
+
+
+    def _validate_epoch(self):
+        # val_dl = torch.utils.data.DataLoader(self.validation_dataset, batch_size=256)
+        num_batches = len(self.validation_dataset)
+        
+        val_losses = 0
+        val_metrics = 0
+        
+        for x_batch, y_batch in self.validation_dataset:
+            loss, metric = self._calculate_validation_loss_metric(x_batch, y_batch)
+            
+            val_losses += loss
+            val_metrics += metric
+        
+        epoch_val_loss = round(val_losses / num_batches, 4)
+        epoch_metric = round(val_metrics / num_batches, 4)
+        
+        self.epoch_validation_loss.append(epoch_val_loss)
+        self.epoch_metrics.append(epoch_metric)
+
+    
+    def fit(self, epochs, lr=0.1):
+        """Fits network parameters to best fit inputs and outputs over epochs loops using lr as learning rate.
+        """
+        self.optimizer = self.optimizer_class(self.model.parameters(), lr)
+        
+        for i in range(epochs):
+            self._train_epoch()
+            self._validate_epoch()
+            
+            print(f"{self.epoch_training_loss[-1]} | {self.epoch_validation_loss[-1]} | {self.epoch_metrics[-1]}")
+        
---
+++ mnist_learner_test.py
@@ -0,0 +1,102 @@
+import torch
+from torchvision.datasets import MNIST
+import torchvision.transforms as T
+from mnist_learner import Learner
+
+from pathlib import Path
+
+# Train Loop Functions
+
+class BasicOptimizer:
+    """Basic Optimizer that can update parameters using gradients."""
+    
+    def __init__(self, parameters, learning_rate):
+        self.parameters = parameters
+        self.learning_rate = learning_rate
+        
+    def step(self):
+        for p in self.parameters:
+            p.data -= self.learning_rate * p.grad.data
+    
+    def zero_grad(self):
+        for p in self.parameters:
+            p.grad = None
+
+
+def mnist_loss(predictions, targets):
+    predictions = predictions.sigmoid()
+    
+    # loss = (((1 - predictions) ** targets) * (predictions ** (1 - targets))).mean()
+    loss = torch.where(targets == 1, 1 - predictions, predictions).mean()
+    return loss
+    
+
+def batch_accuracy(predictions, targets):
+    predictions = predictions.sigmoid()
+    
+    accuracy = ((predictions > 0.5) == targets).float().mean()
+    
+    return accuracy
+
+
+# Data Loading Functions
+
+def dataset_load_label_mask(label):
+    if label == 3:
+        return 1
+    elif label == 7:
+        return 0
+    else: 
+        return -1
+
+
+def create_image_dataset_mask(dataset, mask_vals):
+    mask = dataset.targets != dataset.targets
+    
+    for mask_val in mask_vals:
+        mask |= dataset.targets == mask_val
+        
+    return mask
+
+
+if __name__ == '__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    kwargs = {'num_workers': 1, 'pin_memory': True} if device == 'cuda' else {}
+    
+    
+    
+    train_mnist_images = MNIST(
+        Path('Datasets'), train=True, 
+        transform=T.Compose(
+            [
+                T.ToTensor(), T.Lambda(lambda x: x.flatten(start_dim=-2).squeeze())
+            ]
+        ),
+        target_transform=dataset_load_label_mask
+    )
+    
+    valid_mnist_images = MNIST(
+        Path('Datasets'), train=False, 
+        transform=T.Compose(
+            [
+                T.ToTensor(), T.Lambda(lambda x: x.view(-1, 28*28).squeeze())
+            ]
+        ),
+        target_transform=dataset_load_label_mask
+    )
+
+    mask_vals = [3, 7]
+
+    train_mnist_images.data, train_mnist_images.targets = train_mnist_images.data[create_image_dataset_mask(train_mnist_images, mask_vals)], train_mnist_images.targets[create_image_dataset_mask(train_mnist_images, mask_vals)]
+
+    valid_mnist_images.data, valid_mnist_images.targets = valid_mnist_images.data[create_image_dataset_mask(valid_mnist_images, mask_vals)], valid_mnist_images.targets[create_image_dataset_mask(valid_mnist_images, mask_vals)]
+    
+    train_dataset = torch.utils.data.DataLoader(train_mnist_images, batch_size=256, shuffle=True)
+    valid_dataset = torch.utils.data.DataLoader(valid_mnist_images, batch_size=256, shuffle=True)
+    
+    # learn = Learner(train_dataset, valid_dataset, torch.nn.Linear(28*28, 1), opt_func=BasicOptimizer, loss_func=mnist_loss, metrics=batch_accuracy)
+    learn = Learner(train_dataset, valid_dataset, torch.nn.Linear(28*28, 1), opt_func=torch.optim.SGD, loss_func=mnist_loss, metrics=batch_accuracy)
+    
+    learn.fit(10, lr=1)
+    
+    
