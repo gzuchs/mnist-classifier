Fixed an issue that was causing the model gradients to be calculated incorrectly. The MNIST dataset was loading the labels in as single values. When looking at a batch of images and labels, the label is a 1-D tensor batch-size long. The prediction calculation of the image data results in a 2-D tensor with a size of batch-size, 1. This is causing the loss and accuracy functions to broadcast the comparison of predictions and labels to a 2-D tensor with a size of batch-size, batch-size.
Critically, this is causing a loss comparison between all of the predictions and all of the labels in the batch (really bad when trying to compare a 3 image to a 7 label), inflating the mean loss value. The loss is never able to reduce, and the gradients will never properly go toward their optimal values.

Issue was fixed by ensuring the labels are read in as tensors instead of as values, leading to labels in batches taking the shape of a 2-D tensor with a size of batch-size, 1, preventing broadcasting.

mnist_learner_test.py
@@ -43,11 +43,11 @@ def batch_accuracy(predictions, targets):
 
 def dataset_load_label_mask(label):
     if label == 3:
-        return 1
+        return torch.tensor([1])
     elif label == 7:
-        return 0
+        return torch.tensor([0])
     else: 
-        return -1
+        return torch.tensor([-1])
 
 
 def create_image_dataset_mask(dataset, mask_vals):